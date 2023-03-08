906b7598c127bf76f2ec7d80699ba76d674145e3
Jeremy Goodman <jeremygoodman52@gmail.com>
2023-03-08 09:15:53 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace garys_wholesale_garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
